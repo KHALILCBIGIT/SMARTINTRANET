8e83b56970e15917df34bfea801090fa8217a791
KhalilKertaoui <54622656+KhalilKertaoui@users.noreply.github.com>
2020-01-24 15:48:38 +0100

    First Commiit Khalil

---
+++ SmartIntranet/src/main/java/com/gestion/client/SmartIntranetApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIntranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIntranetApplication.class, args);
+	}
+
+}
